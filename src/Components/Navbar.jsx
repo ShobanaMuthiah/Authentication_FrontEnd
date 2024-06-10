d8553ee28bb59307011923503ea96e8296987377
ShobanaMuthiah <shobanam222@gmail.com>
2024-06-09 18:43:12 -1200

    updated

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,8 +19,8 @@ const Navbar = ({isLoggedIn,logout}) => {
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon" />
     </button>
-    <div  >
-      <ul className="collapse navbar-collapse navbar-nav" id="navbarNav">
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
         <li className="nav-item">
         <Link className="nav-link active" aria-current="page" to="/">Home</Link>
           
